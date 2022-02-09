efff18494f5eab7e9c4ad4e1dde3bc3dda7cdd33
Сергей Буркун <GizmoG2@live.ru>
2022-02-09 17:22:45 +0200

    Добавлен перезаруск сцены при поражении и финише.

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class FinishLine : MonoBehaviour
     {
         if (player)
         {
-            Debug.Log("Player Finish!");
+            SceneManager.LoadScene(0);
         }
         else
         {
